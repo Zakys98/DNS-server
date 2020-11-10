e416a047182362476782047cef957953a6a64722
Zakys98 <zakys98@gmail.com>
2020-11-10 23:35:50 +0100

    small fixes

--- test.sh
+++ test.sh
@@ -28,7 +28,7 @@ echo "#################################"
 dig @127.0.0.1 -p 3333 gooasdasdagle.com
 dig @127.0.0.1 -p 3333 www.seznam.cz/adsa
 
-# ./dns -p 3333 -f bad_domain_name_long -s 8.8.8.8 			        ipv4 autorativní    server
-# ./dns -p 3333 -f bad_domain_name_long -s 127.0.0.53			    ipv4 neautorativní  server
-# ./dns -p 3333 -f bad_domain_name_long -s 2001:4860:4860::8844		ipv6 autorativni    server
+# ./dns -p 3333 -f bad_domain_name -s 8.8.8.8 			        ipv4 autorativní    server
+# ./dns -p 3333 -f bad_domain_name -s 127.0.0.53			    ipv4 neautorativní  server
+# ./dns -p 3333 -f bad_domain_name -s 2001:4860:4860::8844		ipv6 autorativni    server
 # valgrind --leak-check=full ./dns -p 3333 -f test_soubor -s 8.8.8.8
